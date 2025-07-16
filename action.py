5bb8036b5bcd9b32b34530648cd8a68dbb37a39c
clbanjai <clbanjai@mit.edu>
2025-07-16 15:51:46 -0400

    full deployment test

--- action.py
+++ action.py
@@ -42,7 +42,7 @@ companies_chunk = companies[start:end]
 final_enriched_companies = []
 total = len(companies_chunk)
 
-for i, row in enumerate(companies_chunk[:20], start=1):
+for i, row in enumerate(companies_chunk, start=1):
     final_enriched_companies.append(affinity_enrich(row,i))
 
 
