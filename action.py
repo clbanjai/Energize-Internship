10503c870cfb24d50cf04de16e163bd1670b87cd
clbanjai <clbanjai@mit.edu>
2025-07-16 14:06:49 -0400

    Github Token method ?

--- action.py
+++ action.py
@@ -43,9 +43,9 @@ companies_chunk = companies[start:end]
 final_enriched_companies = []
 total = len(companies_chunk)
 
-for i, row in enumerate(companies_chunk[:20], start=1):
-    if i % 5 == 0:
-        print(f"Enriching {i}/{total}: {row[0]}")
+for i, row in enumerate(companies_chunk[:5], start=1):
+    # if i % 5 == 0:
+    print(f"Enriching {i}/{total}: {row[0]}")
     final_enriched_companies.append(affinity_enrich(row))
 
 
